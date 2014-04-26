d6dd9c25d3ea8b314938c41d3f9a0c3b89db874c
maxwell9 <maxwell9@uw.edu>
2014-04-26 06:58:47 -0700

    ready for parsing. still only works on people you have messaged within past 8-10 ppl

--- trender.rb
+++ trender.rb
@@ -1,4 +1,4 @@
-require 'koala'
+require 'koala' # facebook wrapper
 require 'open-uri'
 require 'json'
 access_token = 'CAACEdEose0cBAJZCylvBU9cKQnWdZAcPHkvqnZB4z3SxU4OhTnbF8NX4clwIhyOZCadyjN5AfeHqUTiIdP240vMCZBvKmAtJievBXY6MDcu8CK3b7fZBoKvkIZAsroVlfjfk5xYWSmfcJzax2xyfQbr0qfIoROfNX3oppyHodbHGeVbZAAZCZBGy4KuDNHhNw0lZBxM5DgfLMySPAZDZD'
@@ -6,35 +6,38 @@ access_token = 'CAACEdEose0cBAJZCylvBU9cKQnWdZAcPHkvqnZB4z3SxU4OhTnbF8NX4clwIhyO
 if ARGV.size != 1 then
   fail 'missing friend of interest'
 end
-friend = ARGV[0]
-graph = Koala::Facebook::API.new(access_token)
-user  = graph.get_object("me")
-messages = graph.get_connections(user["id"], "outbox", { :limit => 2000})
+friend    = ARGV[0]
+graph     = Koala::Facebook::API.new(access_token)
+user      = graph.get_object("me")
+messages  = graph.get_connections(user["id"], "outbox", { :limit => 2000})
 json_msgs = JSON.parse(JSON.pretty_generate(messages))
-#puts JSON.pretty_generate(messages)
-#exit # this is enough to print out queryable json
 
 # find friend among all friends you've messaged  
 times = []
 target = ""
 json_msgs.each do |msg|
+  # these brackets on brackets are walking down the info tree
+  # (it's JSON, but same idea as XML)
   if msg["to"]["data"][1] and msg["to"]["data"][1]["name"].include? friend then
-    target = msg["comments"]
-    break
+    target = msg["comments"] # Facebook only will have one message set
+    break                    # per friend per output page
   end
 end
+
 # at this point, we never need to find friend again :D
-pages_deep = 0
-msg_dates = []
+# we just follow the next links to the next set of
+# messages by friend.
+pages_deep = 0 # for debug
+msg_dates = [] 
 msg_dates = Hash.new
 while target and target["paging"] do
-  #puts pages_deep.to_s + " pages deep"
-  #pages_deep = pages_deep.next
+  #puts pages_deep.to_s + " pages deep" # debug
+  #pages_deep = pages_deep.next         # debug
   entries = target["data"]
   entries.each do |entry|
-    time = entry["created_time"]
+    time = entry["created_time"] # get timestamp
     date_key = ""
-    time.split("-").each { |item| date_key += item.split("T")[0] }
+    time.split("-").each { |item| date_key += item.split("T")[0] } # to form yyyymmdd
     unless msg_dates.has_key? date_key then msg_dates[date_key] = 0 end
     msg_dates[date_key] = msg_dates[date_key].next
   end
@@ -42,5 +45,6 @@ while target and target["paging"] do
   target = JSON.load(open(next_link))
 end
 data = []
+# not sure if the below prints sorted by date
 msg_dates.each { |date, count| puts "#{date} #{count}" }
-#puts msg_dates #Hash[msg_dates.to_a().reverse()]
+
